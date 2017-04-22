10d11f7e8741c94f45b90fafc5f4ac344220cf1d
YangJingL <YANGjing99>
2017-04-22 08:35:03 +0800

    拖拽

--- src/js/rwb.js
+++ src/js/rwb.js
@@ -9,21 +9,33 @@ $(document).ready(function(){
     $(".fd-rwb-item").draggable({
         helper: "clone"
     });
+    $(".fd-content li").draggable({
+        helper: "clone"
+    });
+    $("#fd-drop-con").droppable({
+        drop: function( event, ui ) {
+
+        }
+    });
     $(document).on("click",".fd-pages a",function(){
         debugger
         var xh = $(this).attr("xh");
         var showUl =  $(this).closest(".fd-rwb-item").find("ul");
-
+        $(this).closest(".fd-rwb-item").addClass("fd-over-hide");
         $(showUl).animate({
             left: "216px",
             opacity:0
-        },100);
-        $(showUl).removeClass("show");
-        $(showUl[xh-1]).addClass("show");
-        $(showUl[xh-1]).animate({
-            left: "0",
-            opacity:1
-        },100);
+        },100,function(){
+            $(showUl).removeClass("show");
+            $(showUl[xh-1]).addClass("show");
+            $(showUl[xh-1]).animate({
+                left: "0",
+                opacity:1
+            },100,function(){
+                $(this).closest(".fd-rwb-item").removeClass("fd-over-hide");
+            });
+        });
+
 
 
 
